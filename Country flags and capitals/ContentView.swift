8b7788e45804deaa874daf49c5167bd8d66634dc
Dimitar Anguelov <d.angelov.9090@gmail.com>
2024-05-03 15:45:05 +0300

    Initial Commit

---
+++ Country flags and capitals/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Country flags and capitals
+//
+//  Created by Dimitar Angelov on 3.05.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Country flags and capitals/Country_flags_and_capitalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Country_flags_and_capitalsApp.swift
+//  Country flags and capitals
+//
+//  Created by Dimitar Angelov on 3.05.24.
+//
+
+import SwiftUI
+
+@main
+struct Country_flags_and_capitalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
