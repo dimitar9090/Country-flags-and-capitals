df75b138ae85cc5ddc9125d0ba28fb96ff5c381f
Dimitar Anguelov <d.angelov.9090@gmail.com>
2024-05-08 00:23:29 +0300

    i add new game guess the capital

---
+++ Country flags and capitals/CapitalGameView.swift
@@ -0,0 +1,18 @@
+//
+//  CapitalGameView.swift
+//  Country flags and capitals
+//
+//  Created by Dimitar Angelov on 8.05.24.
+//
+
+import SwiftUI
+
+struct CapitalGameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CapitalGameView()
+}
---
+++ Country flags and capitals/OpeningPageView.swift
@@ -0,0 +1,18 @@
+//
+//  OpeningPageView.swift
+//  Country flags and capitals
+//
+//  Created by Dimitar Angelov on 8.05.24.
+//
+
+import SwiftUI
+
+struct OpeningPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OpeningPageView()
+}
