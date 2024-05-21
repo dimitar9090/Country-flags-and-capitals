e4a210ee141b7f1e5e31c8dba248fdbf583e0ea0
Dimitar Anguelov <d.angelov.9090@gmail.com>
2024-05-21 20:46:54 +0300

    new view

---
+++ Country flags and capitals/StartView.swift
@@ -0,0 +1,18 @@
+//
+//  StartView.swift
+//  Country flags and capitals
+//
+//  Created by Dimitar Angelov on 21.05.24.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartView()
+}
