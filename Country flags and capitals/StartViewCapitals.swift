4319db617934ac999f9b243b74d083c29404682d
Dimitar Anguelov <d.angelov.9090@gmail.com>
2024-05-21 21:41:31 +0300

    i add start button and for the other view. i fixed but with assets

---
+++ Country flags and capitals/StartViewCapitals.swift
@@ -0,0 +1,18 @@
+//
+//  StartViewCapitals.swift
+//  Country flags and capitals
+//
+//  Created by Dimitar Angelov on 21.05.24.
+//
+
+import SwiftUI
+
+struct StartViewCapitals: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartViewCapitals()
+}
